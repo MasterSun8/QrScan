757ff69b89bc2c5c24259a7cd9399f52af9040f1
MasterSun8 <kacpi.doro@gmail.com>
2023-07-02 16:08:25 +0200

    hmmm

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 body = document.body
 
+function tf(es) {
+    console.log(es)
+    body.innerHTML += es + "<br>"
+}
+
 async function imageDataFromSource(source) {
-    const image = Object.assign(new Image(), { src: source });
+    tf(source)
+    const image = Object.assign(new Image(), { src: source })
     await new Promise(resolve => image.addEventListener('load', () => resolve()));
     const context = Object.assign(document.createElement('canvas'), {
         width: image.width,
         height: image.height
-    }).getContext('2d');
-    context.imageSmoothingEnabled = false;
-    context.drawImage(image, 0, 0);
-    return context.getImageData(0, 0, image.width, image.height);
-}
-
-function tf(es) {
-    console.log(es)
-    body.innerHTML += es + "<br>"
+    }).getContext('2d')
+    context.imageSmoothingEnabled = false
+    context.drawImage(image, 0, 0)
+    return context.getImageData(0, 0, image.width, image.height)
 }
 
 const imageEl = await imageDataFromSource('qr.png')
