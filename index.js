6c1fe025503352c06c3d322cb4677f46924829d1
MasterSun8 <kacpi.doro@gmail.com>
2023-07-02 16:06:13 +0200

    will it work?

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 body = document.body
-const imageEl = new Image(100, 200);
-imageEl.src = "qr.png";
+
+async function imageDataFromSource(source) {
+    const image = Object.assign(new Image(), { src: source });
+    await new Promise(resolve => image.addEventListener('load', () => resolve()));
+    const context = Object.assign(document.createElement('canvas'), {
+        width: image.width,
+        height: image.height
+    }).getContext('2d');
+    context.imageSmoothingEnabled = false;
+    context.drawImage(image, 0, 0);
+    return context.getImageData(0, 0, image.width, image.height);
+}
 
 function tf(es) {
     console.log(es)
     body.innerHTML += es + "<br>"
 }
+
+const imageEl = await imageDataFromSource('qr.png')
 
 tf(imageEl)
 
