30a5c93ead91ced233ef1cb9f8fa692519ae0242
MasterSun8 <kacpi.doro@gmail.com>
2023-07-04 16:59:44 +0200

    esa

--- index.js
+++ index.js
@@ -3,33 +3,19 @@ const img = document.createElement("img")
 img.src = "qr.jpg"
 img.id = "code"
 body.appendChild(img)
+tf()
 
 let elem = document.querySelectorAll("img")[0]
 
-function tf(es) {
+functiontf(es = ""){
     console.log(es)
     body.innerHTML += es + "<br>"
 }
 
-async function getImage() {
+window.onload = () => {
     try {
-        let x = await fetch("qr.jpg")
-        if (x.ok) {
-            tf(x)
-        } else {
-            tf(x?.status)
-            tf(x?.statusText)
-        }
-        x = await x.blob()
-        return x
-    } catch (err) {
-        tf(err)
-    }
-}
-
-try {
-  tf(elem)
-        let barcodeDetector = new BarcodeDetector()
+        tf(elem)
+        let barcodeDetector = newBarcodeDetector()
 
         barcodeDetector
             .detect(elem)
@@ -39,6 +25,7 @@ try {
             .catch((err) => {
                 tf(err)
             })
-} catch (err) {
-    tf(err)
+    } catch (err) {
+        tf(err)
+    }
 }
