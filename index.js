24aa1f957e6af63a40970775cea983c73df6daf5
MasterSun8 <kacpi.doro@gmail.com>
2023-07-04 13:24:22 +0200

    maybe

--- index.js
+++ index.js
@@ -27,7 +27,21 @@ try {
     image.then(val => {
         tf(val)
         let barcodeDetector = new BarcodeDetector({
-            formats: ["code_39", "codabar", "ean_13"],
+            formats: [
+                "aztec",
+                "code_128",
+                "code_39",
+                "code_93",
+                "codabar",
+                "data_matrix",
+                "ean_13",
+                "ean_8",
+                "itf",
+                "pdf417",
+                "qr_code",
+                "upc_a",
+                "upc_e"
+            ],
         })
 
         barcodeDetector
